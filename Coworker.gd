3d5981da9be179b521ff56055986ec22be87efe2
Benoît Richard <kolaru@hotmail.com>
2020-07-11 16:03:10 +0200

    Basic coworker implementation

---
+++ Coworker.gd
@@ -0,0 +1,25 @@
+extends PanelContainer
+
+
+func _ready():
+	$SpeakTimer.wait_time = randf() * 4
+	$SpeakTimer.start()
+
+
+func speak(text):
+	$BubbleCanvas.offset = rect_position + rect_size/2
+	$BubbleCanvas/Bubble/Text.text = text
+	$BubbleCanvas/Bubble/Timer.start()
+	$BubbleCanvas/Bubble.visible = true
+
+
+func stop_speaking():
+	$BubbleCanvas/Bubble.visible = false
+
+
+func _on_bubble_timer_timeout():
+	stop_speaking()
+
+
+func _on_speaktimer_timeout():
+	speak("Please manage me " + self.name)
