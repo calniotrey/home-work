20e96fdde9b46fc8c3d469830ccd0a6ef8cdca97
Benoît Richard <kolaru@hotmail.com>
2020-07-12 00:39:48 +0200

    Add generation option to avatars

--- scenes/Avatar.gd
+++ scenes/Avatar.gd
@@ -1,21 +1,60 @@
 extends PanelContainer
 
 
+var eye_id = 0
+var hair_id = 0
+var symbol_id = 0
+var shirt_color = Color(1, 1, 1)
+
+
 func _ready():
 	generate_face()
 
 
+func hide_all():
+	hide_feature($Eyes)
+	hide_feature($Hairs)
+	hide_feature($Symbols)
+
+
+func hide_feature(node):
+	for child in node.get_children():
+		child.visible = false
+
+
 func generate_face():
-	var eyes = $Eyes.get_children()
+	hide_all()
 	
-	for eye in eyes:
-		eye.visible = false
-	
-	var k = randi() % len(eyes)
-	eyes[k].visible = true
-
-
-func copy(avatarToCopy):
 	var eyes = $Eyes.get_children()
-	for i in range(len(eyes)):
-		eyes[i].visible = avatarToCopy.get_node("Eyes").get_children()[i].visible
+	eye_id = randi() % len(eyes)
+	eyes[eye_id].visible = true
+	
+	var hairs = $Hairs.get_children()
+	hair_id = randi() % len(hairs)
+	hairs[hair_id].visible = true
+	
+	var symbols = $Symbols.get_children()
+	symbol_id = randi() % len(symbols)
+	symbols[symbol_id].visible = true
+	
+	shirt_color = Color(randf(), randf(), randf())
+	$Shirt.modulate = shirt_color
+	
+
+func copy(avatar_to_copy):
+	hide_all()
+	var eyes = $Eyes.get_children()
+	eye_id = avatar_to_copy.eye_id
+	eyes[eye_id].visible = true
+	
+	var hairs = $Hairs.get_children()
+	hair_id = avatar_to_copy.hair_id
+	hairs[hair_id].visible = true
+	
+	var symbols = $Symbols.get_children()
+	symbol_id = avatar_to_copy.symbol_id
+	symbols[symbol_id].visible = true
+	
+	shirt_color = avatar_to_copy.shirt_color
+	$Shirt.modulate = shirt_color
+
