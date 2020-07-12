771d27882620f43385e7f6dd62fa445c8697a0cd
Benoît Richard <kolaru@hotmail.com>
2020-07-12 19:47:19 +0200

    Add dollars to victory screen

--- scripts/Victory.gd
+++ scripts/Victory.gd
@@ -2,5 +2,14 @@ extends Control
 
 onready var next_scene = preload("res://scenes/MainMenu.tscn")
 
+
+func _ready():
+	$MoneyParticle.emission_rect_extents.x = rect_size.x
+
+
 func _on_continue():
 	get_tree().change_scene_to(next_scene)
+
+
+func _on_resized():
+	$MoneyParticle.emission_rect_extents.x = rect_size.x
