29f60086bc64b7551207986ea9004b813d93451f
Benoît Richard <kolaru@hotmail.com>
2020-07-12 02:08:16 +0200

    Resizable main menu

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -4,5 +4,5 @@ var next_scene = preload("res://scenes/Main.tscn")
 
 func _on_StartGame_pressed():
 	var game_data = get_node("/root/GameData")
-	game_data.difficulty = $Panel/Difficulty.selected
+	game_data.difficulty = $Difficulty.selected
 	get_tree().change_scene_to(next_scene)
